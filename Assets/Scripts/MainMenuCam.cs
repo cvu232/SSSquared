75eaf30c5c99e8d24a6b65ac4a25dac9f78a50ac
Juan Callejas <jcallejasrodriguez@gmail.com>
2021-04-09 21:20:56 -0500

    Added main menu appearing animation

---
+++ Assets/Scripts/MainMenuCam.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (Camera))]
+public class MainMenuCam : MonoBehaviour {
+
+    public float speed;
+
+    private float finalFOV;
+
+    private Camera cam;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        cam = GetComponent<Camera>();
+
+        finalFOV = cam.fieldOfView;
+
+        cam.fieldOfView = 1;
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        if (cam.fieldOfView >= finalFOV)
+            speed = Mathf.Lerp(speed, 0, Time.deltaTime * 0.1f);
+
+        cam.fieldOfView = cam.fieldOfView + (speed * Time.deltaTime);
+
+    }
+
+}
