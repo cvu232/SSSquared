20abd02dd5f057db5a077eaf7ccde0faaacb57e1
Juan Callejas <jcallejasrodriguez@gmail.com>
2021-04-10 00:22:59 -0500

    Player profiles

---
+++ Assets/Scripts/PlayerProfile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerProfile : MonoBehaviour {
+
+    public int activePlayer;
+
+    public Image bg;
+    public Color[] BGColors;
+    public GameObject[] profiles;
+
+    // Start is called before the first frame update
+    void Start() {
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        bg.color = Color.Lerp(bg.color, BGColors[activePlayer], Time.deltaTime * 5);
+
+        for (int i = 0; i < profiles.Length; i++) {
+
+            profiles[i].SetActive(i == activePlayer);
+
+        }
+
+    }
+
+}
---
+++ Assets/Scripts/PlayerProfileAnimShuffler.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent (typeof (Animator))]
+public class PlayerProfileAnimShuffler : MonoBehaviour {
+
+    public bool hasVariant;
+
+    public float idleSwitchMin;
+    public float idleSwitchMax;
+    public float variantMin;
+    public float variantMax;
+
+    private Animator anim;
+    private float idleSwitchTimer;
+    private float variantTimer;
+    private bool inVariant;
+
+    // Start is called before the first frame update
+    void Start() {
+
+        anim = GetComponent<Animator>();
+
+        idleSwitchTimer = Random.Range(idleSwitchMin, idleSwitchMax);
+        variantTimer = Random.Range(variantMin, variantMax);
+
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+        idleSwitchTimer -= Time.deltaTime;
+        if (!inVariant)
+            variantTimer -= Time.deltaTime;
+        else
+            variantTimer = Random.Range(variantMin, variantMax);
+
+        if (hasVariant) {
+            if (idleSwitchTimer <= 0) {
+                idleSwitchTimer = Random.Range(idleSwitchMin, idleSwitchMax);
+                inVariant = !inVariant;
+                anim.SetBool("IdleVariant", inVariant);
+                anim.ResetTrigger("Variant1");
+                anim.ResetTrigger("Variant2");
+            }
+        } else
+            inVariant = false;
+
+        if (variantTimer <= 0) {
+            variantTimer = Random.Range(variantMin, variantMax);
+            anim.SetTrigger("Variant" + (Random.Range(0, 2) == 0 ? "1" : "2"));
+        }
+
+    }
+
+}
