6e84780f823e28c57d8ef4e2a1f24af2acc02585
Juan Callejas <jcallejasrodriguez@gmail.com>
2021-04-12 05:45:49 -0500

    ADded teleporter and sidespring, increased sidepring bounce force and added upward force

--- Assets/Scripts/SideSpring.cs
+++ Assets/Scripts/SideSpring.cs
@@ -12,7 +12,7 @@ public class SideSpring : ObjectEffect
             Rigidbody2D rb = other.attachedRigidbody;
             rb.velocity = new Vector2(rb.velocity.x, 0); // reset y velo
             int dir = Math.Sign(rb.position.x - transform.position.x);
-            rb.AddForce(new Vector2(dir, 0) * 40, ForceMode2D.Impulse);
+            rb.AddForce((new Vector2(dir, 0) * 60) + (Vector2.up * 50), ForceMode2D.Impulse);
         }
     }
 }
